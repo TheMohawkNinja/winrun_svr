f1065b045bb027321f9511a956869b3cf6d5f5f6
Benjamin Dennison <Ben@BenPC.Ninja.local>
2020-09-16 18:29:15 -0500

    Add project files.

---
+++ winrun_svr/main.cpp
@@ -0,0 +1,178 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+#include <time.h> 
+
+#pragma comment (lib,"ws2_32.lib")
+
+using namespace std;
+
+const int bufsize = 4096;
+string id;
+
+void getCmdOut(string cmd, int s, string b)
+{
+	int line = 0;
+	string data;
+	FILE* stream;
+	char buffer[bufsize];
+	char recbuffer[bufsize];
+	cmd.append(" 2>&1");
+	stream = _popen(cmd.c_str(), "r");
+
+	//Send id
+	send(s, id.c_str(), id.length() + 1, 0);
+
+	if (stream)
+	{
+		while (!feof(stream))
+		{
+			//cout<<fgets(buffer,max_buffer,stream);
+			if (fgets(buffer, bufsize, stream) != NULL)
+			{
+				data.append(buffer);
+
+				if (data.find("\n") != std::string::npos)
+				{
+					data = std::to_string(line) + "-" + data;
+					send(s, data.c_str(), data.length() + 1, 0);
+					cout << "\t" + data;
+					data = "";
+					line++;
+
+					ZeroMemory(recbuffer, bufsize);
+					recv(s, recbuffer, bufsize, 0);
+				}
+			}
+		}
+
+		//Send break code to client to signal completion of command execution
+		send(s, b.c_str(), (b.length() + 1), 0);
+
+		_pclose(stream);
+	}
+}
+int main()
+{
+	//Initialize winsock
+	WSADATA wsData;
+	WORD ver = MAKEWORD(2, 2);
+	int wsok = WSAStartup(ver, &wsData);
+	string output, breakCode, command;
+
+	if (wsok != 0)
+	{
+		cerr << "Unable to initialize Winsock! Quitting..." << endl;
+		return -1;
+	}
+	else
+	{
+		cout << "Inititalized Winsock" << endl;
+	}
+	
+	//Create socket
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+	if (listening == INVALID_SOCKET)
+	{
+		cerr << "Unable to create socket! Quitting..." << endl;
+		return -2;
+	}
+	else
+	{
+		cout << "Created socket " + to_string(listening) << endl;
+	}
+
+	//Bind IP and port to socket
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(55000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY; //Could also use inet_pton
+	
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+	//Tell winsock that socket is for listening
+	cout << "Waiting for client to connect..." << endl;
+	listen(listening, SOMAXCONN);
+
+	//Wait for connection
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+
+	if (clientSocket == INVALID_SOCKET)
+	{
+		cerr << "Invalid socket! Quitting..." << endl;
+		return -3;
+	}
+
+	char host[NI_MAXHOST];		//Client name
+	char service[NI_MAXSERV];	//Service the client is connected on
+
+	ZeroMemory(host, NI_MAXHOST);	//Use memset(host,0,NI_MAXHOST) for Linux 
+	ZeroMemory(service, NI_MAXSERV);
+
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0) //Attempt to resolve client machine name, otherwise resort to IP
+	{
+		cout << endl;
+		cout << host << " connected on port " << service << endl;
+	}
+	else
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		cout << endl;
+		cout << host << " connected on port " << ntohs(client.sin_port) << endl;
+	}
+
+	//Close listening socket
+	closesocket(listening);
+
+	//Generate 64 digit code to signal the completion of the command execution and inform client.
+	srand(time(NULL));
+	for (int i = 0; i < 64; i++)
+	{
+		breakCode += to_string(rand() % 10);
+	}
+	cout << "breakCode = " + breakCode << endl;
+	send(clientSocket, breakCode.c_str(), (breakCode.length() + 1), 0);
+
+	//Main loop for running commands
+	while (true)
+	{
+		//Get data and run command
+		char buf[bufsize];
+		ZeroMemory(buf, bufsize);
+
+		//Wait for client to send data
+		int bytesReceived = recv(clientSocket, buf, bufsize, 0);
+
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			cout << "Socket Error recieved, did winrund stop?" << endl;
+		}
+		else if (bytesReceived == 0)
+		{
+			cout << "Client disconected." << endl;
+		}
+		else
+		{
+			//Run command and echo output back to client
+			id = string(buf, 0, bytesReceived).substr(0, string(buf, 0, bytesReceived).find("\""));
+			cout << "Recieved command for ID: " + id << endl;
+
+			command = string(buf, 0, bytesReceived).substr(id.length(), (string(buf, 0, bytesReceived).length() - id.length())).c_str();
+			cout << "Running command " + command << endl;
+			getCmdOut(command, clientSocket, breakCode);
+
+			cout << "Command completed!" << endl;
+		}
+	}
+
+	//Close socket
+	cout << "Closing socket..." << endl;
+	closesocket(clientSocket);
+
+	//Cleanup winsock
+	WSACleanup();
+
+	return 0;
+}
